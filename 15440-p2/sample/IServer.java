381400360a56648c64b0b601e4fa0658027742c5
Dan Cheng <dancheng333@Dans-MacBook.wv.cc.cmu.edu>
2017-03-08 11:18:17 -0500

    modified

--- 15440-p2/sample/IServer.java
+++ 15440-p2/sample/IServer.java
@@ -5,6 +5,6 @@ public interface IServer extends Remote {
     String sayHello() throws RemoteException;
     byte[] downloadFile(String path) throws RemoteException;
     byte[] uploadFile(String path, byte[] buffer, int versionNum) throws RemoteException;
-    void initVersionNum(String path);
-    int getVersionNum(String path);
+    void initVersionNum(String path) throws RemoteException;
+    int getVersionNum(String path) throws RemoteException;
 }
