aba0b3aa0ec4031e4adf1a5ece1ee922530167ae
Dan Cheng <dancheng333@Dans-MacBook.wv.cc.cmu.edu>
2017-03-08 19:37:55 -0500

    modified

---
+++ 15440-p2/sample/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+	public static void main(String[] args) {
+		String s = "Hello.txt";
+		String[] sp = s.split("[.]");
+		System.out.println("first:"+sp[0]+" second:"+sp[1]);
+	}
+	
+}
