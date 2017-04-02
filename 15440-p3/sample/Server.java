207bf27fc637e5965b6217997bec2fdff115a39b
Dan Cheng <dancheng333@Dans-MacBook.wv.cc.cmu.edu>
2017-04-02 16:22:08 -0400

    test

--- 15440-p3/sample/Server.java
+++ 15440-p3/sample/Server.java
@@ -36,17 +36,17 @@ public class Server extends UnicastRemoteObject implements IServer {
 		middleServerList = Collections.synchronizedList(new ArrayList<>());
 		requestQueue = new ConcurrentLinkedQueue<Cloud.FrontEndOps.Request>();
 		
-        
+		System.err.println("WHile1");
 		while(SL.getQueueLength() == 0 );
         long time1 = System.currentTimeMillis();
         
         SL.dropHead();
         SL.unregister_frontend();
-        
+        System.err.println("WHile2");
         while(SL.getQueueLength() == 0 );
         long time2 = System.currentTimeMillis();
         long interval = time2 - time1;
-       
+        System.err.println("WHile");
 
        /* if (interval < 100) {
             startNum = 7;
