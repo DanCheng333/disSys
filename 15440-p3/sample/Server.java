086985d470f8157d3632079fa5a22cdce4573d75
Dan Cheng <dancheng333@Dans-MacBook.wv.cc.cmu.edu>
2017-04-08 22:26:22 -0400

    test

--- 15440-p3/sample/Server.java
+++ 15440-p3/sample/Server.java
@@ -148,7 +148,7 @@ public class Server extends UnicastRemoteObject implements IServer {
 
 			// Not going to finish in time.... drop and avoid erroneous sales
 			if (requestQueue.size() > middleServerList.size()) {
-				while (requestQueue.size() > middleServerList.size() * 1.8) {
+				while (requestQueue.size() > middleServerList.size() * 2) {
 					SL.drop(requestQueue.poll());
 					//scaleOut(1,0);
 				}
