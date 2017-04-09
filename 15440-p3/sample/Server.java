1e5ff6740f34d68903a8f0448bb676e5fe7c9b2e
Dan Cheng <dancheng333@Dans-MacBook.wv.cc.cmu.edu>
2017-04-08 20:02:11 -0400

    a

--- 15440-p3/sample/Server.java
+++ 15440-p3/sample/Server.java
@@ -132,12 +132,11 @@ public class Server extends UnicastRemoteObject implements IServer {
 						front = 1;
 						scaleOutCounter = 0;
 					}
-					if (scaleOutCounter % 501 == 0) {
 					System.err.println("fall in range 1.5");
 					int offset = (int) (requestQueue.size() / middleServerList.size() * 5);
 					System.err.println("!!!!!!!!Add middle tiers!!!!!!!!!! offset : " + offset);
 					scaleOut(offset, front);
-					}
+					
 					
 				}
 
